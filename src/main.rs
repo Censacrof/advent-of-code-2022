7e218a3c1b881d539a3b24ebe6cd9a4ffbce8305
Francesco Galisi <galisifrancesco@gmail.com>
2023-08-20 21:29:38 +0200

    case 01 passes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::cmp::max;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn calculacte_most_calories(calories_list: &str) -> i32 {
+    let lines = calories_list.split("\n").collect::<Vec<&str>>();
+
+    let mut max_cals: i32 = 0;
+    let mut acc: i32 = 0;
+    for line in lines {
+        match line.parse::<i32>() {
+            Err(_) => {
+                max_cals = max(acc, max_cals);
+                acc = 0;
+                continue;
+            }
+            Ok(cals) => {
+                acc += cals;
+            }
+        }
+    }
+
+    return max(acc, max_cals);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_01() {
+        let result = calculacte_most_calories(
+            "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n",
+        );
+
+        assert_eq!(result, 24000);
+    }
 }
