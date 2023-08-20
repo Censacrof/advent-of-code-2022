a0324a6fc192a562f91f1bb03656b8dbd2ca4809
Francesco Galisi <galisifrancesco@gmail.com>
2023-08-20 21:45:56 +0200

    Rename test

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn case_01() {
+    fn calculate_most_calories() {
         let result = calculacte_most_calories(
             "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n",
         );
