4e350ec29508641cf3c21b3428f730ef420d0a98
Francesco Galisi <galisifrancesco@gmail.com>
2023-08-21 00:19:08 +0200

    [SOLVES d02] Created exec for d02

--- src/d01.rs
+++ src/d01.rs
@@ -1,11 +1,7 @@
-mod lib;
-
 use std::{
     cmp::max,
     io::{self, Read},
 };
-
-use lib::binary_search_tree::BinaryTree;
 
 fn main() -> io::Result<()> {
     let mut buffer: String = String::new();
@@ -38,45 +34,6 @@ fn calculate_most_calories(calories_list: &str) -> i32 {
     return max(acc, max_cals);
 }
 
-fn calculate_top_3_calories(calories_list: &str) -> (Option<i32>, Option<i32>, Option<i32>) {
-    let lines = calories_list.split("\n").collect::<Vec<&str>>();
-
-    let mut tree: Option<BinaryTree> = None;
-    let mut acc: i32 = 0;
-    for line in lines {
-        match line.parse::<i32>() {
-            Err(_) => {
-                match &mut tree {
-                    None => {
-                        tree = Some(BinaryTree::new(acc));
-                    }
-                    Some(t) => t.binary_insert_desc(acc),
-                }
-
-                acc = 0
-            }
-            Ok(cals) => {
-                acc += cals;
-            }
-        }
-    }
-
-    match tree {
-        Some(t) => {
-            let traversed = t.traverse();
-
-            println!("{:?}", traversed);
-
-            (
-                traversed.get(0).copied(),
-                traversed.get(1).copied(),
-                traversed.get(2).copied(),
-            )
-        }
-        None => (None, None, None),
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -88,14 +45,5 @@ mod tests {
         );
 
         assert_eq!(result, 24000);
-    }
-
-    #[test]
-    fn test_calculate_top_3_calories() {
-        let result = calculate_top_3_calories(
-            "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n",
-        );
-
-        assert_eq!(result, (Some(24000), Some(11000), Some(10000),));
     }
 }
---
+++ src/d02.rs
@@ -0,0 +1,67 @@
+mod lib;
+
+use std::io::{self, Read};
+
+use lib::binary_search_tree::BinaryTree;
+
+fn main() -> io::Result<()> {
+    let mut buffer: String = String::new();
+    let _bytes_read = io::stdin().read_to_string(&mut buffer);
+
+    let (t1, t2, t3) = calculate_top_3_calories(&buffer);
+
+    println!("{}", t1.unwrap() + t2.unwrap() + t3.unwrap());
+
+    Ok(())
+}
+
+fn calculate_top_3_calories(calories_list: &str) -> (Option<i32>, Option<i32>, Option<i32>) {
+    let lines = calories_list.split("\n").collect::<Vec<&str>>();
+
+    let mut tree: Option<BinaryTree> = None;
+    let mut acc: i32 = 0;
+    for line in lines {
+        match line.parse::<i32>() {
+            Err(_) => {
+                match &mut tree {
+                    None => {
+                        tree = Some(BinaryTree::new(acc));
+                    }
+                    Some(t) => t.binary_insert_desc(acc),
+                }
+
+                acc = 0
+            }
+            Ok(cals) => {
+                acc += cals;
+            }
+        }
+    }
+
+    match tree {
+        Some(t) => {
+            let traversed = t.traverse();
+
+            (
+                traversed.get(0).copied(),
+                traversed.get(1).copied(),
+                traversed.get(2).copied(),
+            )
+        }
+        None => (None, None, None),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_calculate_top_3_calories() {
+        let result = calculate_top_3_calories(
+            "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000\n",
+        );
+
+        assert_eq!(result, (Some(24000), Some(11000), Some(10000),));
+    }
+}
