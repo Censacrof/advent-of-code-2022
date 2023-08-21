d8deb408d0f2393a16634c2be66f0a5b9966de3b
Francesco Galisi <francesco.galisi@imagicle.com>
2023-08-21 20:33:47 +0200

    Add lookup tables

--- src/d02/p01.rs
+++ src/d02/p01.rs
@@ -1,4 +1,7 @@
-use std::io::{self, Read};
+use std::{
+    collections::HashMap,
+    io::{self, Read},
+};
 
 fn main() -> io::Result<()> {
     let mut buffer: String = String::new();
@@ -9,8 +12,82 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn calculate_score(moves: &str) -> i32 {
-    unimplemented!();
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+enum Move {
+    Rock,
+    Paper,
+    Scissor,
+}
+
+use Move::*;
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+enum Outcome {
+    Win,
+    Lose,
+    Draw,
+}
+
+use Outcome::*;
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+enum ScoreLookupKey {
+    MoveScore(Move),
+    OutcomeScore(Outcome),
+}
+
+use ScoreLookupKey::*;
+
+fn calculate_score(input: &str) -> i32 {
+    let move_lookup: HashMap<char, Move> = HashMap::from([
+        ('A', Rock),
+        ('B', Paper),
+        ('C', Scissor),
+        ('X', Rock),
+        ('Y', Paper),
+        ('Z', Scissor),
+    ]);
+
+    let outcome_lookup: HashMap<(Move, Move), Outcome> = HashMap::from([
+        ((Rock, Rock), Draw),
+        ((Rock, Paper), Win),
+        ((Rock, Scissor), Lose),
+        ((Paper, Rock), Lose),
+        ((Paper, Paper), Draw),
+        ((Paper, Scissor), Win),
+        ((Scissor, Rock), Win),
+        ((Scissor, Paper), Lose),
+        ((Scissor, Scissor), Draw),
+    ]);
+
+    let score_lookup: HashMap<ScoreLookupKey, i32> = HashMap::from([
+        (MoveScore(Rock), 1),
+        (MoveScore(Paper), 2),
+        (MoveScore(Scissor), 3),
+        (OutcomeScore(Lose), 0),
+        (OutcomeScore(Draw), 3),
+        (OutcomeScore(Draw), 6),
+    ]);
+
+    let lines = input.lines();
+
+    let mut tot = 0;
+    for line in lines {
+        let opponent_char = line.chars().nth(0).unwrap();
+        let our_char = line.chars().nth(2).unwrap();
+
+        let opponent_move = move_lookup.get(&opponent_char).unwrap();
+        let our_move = move_lookup.get(&our_char).unwrap();
+
+        let outcome = outcome_lookup.get(&(*opponent_move, *our_move)).unwrap();
+
+        let round_score = score_lookup.get(&OutcomeScore(*outcome)).unwrap()
+            + score_lookup.get(&MoveScore(*our_move)).unwrap();
+
+        tot += round_score;
+    }
+
+    return tot;
 }
 
 #[cfg(test)]
