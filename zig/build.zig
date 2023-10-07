41ff27ae37a30161fd11d11c6500134e0c53bc52
Francesco Galisi <galisifrancesco@gmail.com>
2023-10-07 18:30:19 +0200

    Improve build file

--- zig/build.zig
+++ zig/build.zig
@@ -1,25 +1,24 @@
 const std = @import("std");
 
+const Build = std.Build;
+const allocator = std.heap.page_allocator;
+const ArrayList = std.ArrayList;
+
+const ExecRun = struct {
+    name: []const u8,
+    run: *std.Build.Step.Run,
+    run_unit_tests: *std.Build.Step.Run,
+};
+
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    const optimize = b.standardOptimizeOption(.{});
-
+fn addExecPath(b: *std.Build, target: std.zig.CrossTarget, optimize: std.builtin.OptimizeMode, path: []const u8, name: []const u8) ExecRun {
     const exe = b.addExecutable(.{
-        .name = "zig",
+        .name = name,
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = path },
         .target = target,
         .optimize = optimize,
     });
@@ -46,25 +45,52 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = path },
         .target = target,
         .optimize = optimize,
     });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
 
+    return ExecRun{
+        .name = name,
+        .run = run_cmd,
+        .run_unit_tests = run_unit_tests,
+    };
+}
+
+pub fn build(b: *std.Build) !void {
+    // Standard target options allows the person running `zig build` to choose
+    // what target to build for. Here we do not override the defaults, which
+    // means any target is allowed, and the default is native. Other options
+    // for restricting supported target set are available.
+    const target = b.standardTargetOptions(.{});
+
+    // Standard optimization options allow the person running `zig build` to select
+    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
+    // set a preferred release mode, allowing the user to decide how to optimize.
+    const optimize = b.standardOptimizeOption(.{});
+
+    var execRuns = ArrayList(ExecRun).init(allocator);
+    defer execRuns.deinit();
+
+    try execRuns.append(addExecPath(b, target, optimize, "src/d01/p01.zig", "d01p01"));
+    try execRuns.append(addExecPath(b, target, optimize, "src/d01/p02.zig", "d01p02"));
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    for (execRuns.items) |execRun| {
+        run_step.dependOn(&execRun.run.step);
+
+        test_step.dependOn(&execRun.run_unit_tests.step);
+    }
 }
---
+++ zig/src/d01/p02.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+
+pub fn main() !void {
+    // stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    var inputFile = try std.fs.cwd().openFile("input.txt", .{ .mode = .read_only });
+    defer inputFile.close();
+
+    const file_size = (try inputFile.stat()).size;
+    const allocator = std.heap.page_allocator;
+
+    var buffer = try allocator.alloc(u8, file_size);
+    defer allocator.free(buffer);
+    try inputFile.reader().readNoEof(buffer);
+
+    const result = get_most_calories(buffer);
+
+    try stdout.print("{d}\n", .{result});
+
+    try bw.flush(); // don't forget to flush!
+}
+
+fn get_most_calories(caloriesList: []const u8) i32 {
+    var lines = std.mem.split(u8, caloriesList, "\n");
+
+    var max: i32 = 0;
+    var acc: i32 = 0;
+    while (lines.next()) |line| {
+        const cals = std.fmt.parseInt(i32, line, 10) catch {
+            max = @max(max, acc);
+            acc = 0;
+            continue;
+        };
+        acc += cals;
+    }
+
+    max = @max(max, acc);
+    return max;
+}
+
+test "case 0" {
+    const input =
+        \\1000
+        \\2000
+        \\3000
+        \\
+        \\4000
+        \\
+        \\5000
+        \\6000
+        \\
+        \\7000
+        \\8000
+        \\9000
+        \\
+        \\10000
+    ;
+
+    try std.testing.expectEqual(get_most_calories(input), @as(i32, 24000));
+}
